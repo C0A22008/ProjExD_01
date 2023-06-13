bc3b0e8fbe63a64f86288be55072fad17ab51266
Garashi <c0a2200830@edu.teu.ac.jp>
2023-06-13 16:51:08 +0900

    演習1

--- ex01-20230613/pygame_image.py
+++ ex01-20230613/pygame_image.py
@@ -9,8 +9,10 @@ def main():
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
     bgReverse_img = pg.transform.flip(bg_img, True, False)
     koukaton_img = pg.image.load("ex01-20230613/fig/3.png")
+    koukaton2_img = pg.image.load("ex01-20230613/fig/4.png")
+    koukaton2Reverse_img = pg.transform.flip(koukaton2_img, True, False)
     koukatonReverse_img = pg.transform.flip(koukaton_img, True, False)
-    koukatonReverseRotate10_img = [koukatonReverse_img, pg.transform.rotate(koukatonReverse_img, 10)]
+    koukatonReverseRotate10_img = [koukaton2Reverse_img, pg.transform.rotate(koukatonReverse_img, 10)]
     x = 0
     x2 = 0
 
@@ -25,16 +27,21 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bgReverse_img, [1600-x2, 0])
         screen.blit(bg_img, [3200-x2, 0])
-        screen.blit(koukatonReverseRotate10_img[tmr % 2], [300, 200])
+        if tmr <= 40:
+            screen.blit(koukatonReverseRotate10_img[0], [300, 200])
+        else:
+            screen.blit(koukatonReverseRotate10_img[1], [300, 200])
         pg.display.update()
         tmr += 1
+        if tmr >= 80:
+            tmr=0
         x += 1
         if x >= 1600:
             x = 0
         x2 += 1
         if x2 >= 3200:
             x2 -= 3200     
-        clock.tick(1000)
+        clock.tick(200)
 
 
 
