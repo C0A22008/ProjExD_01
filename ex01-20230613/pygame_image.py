78961f36ab8d618119f9be7288195d24f9bedda3
Garashi <c0a2200830@edu.teu.ac.jp>
2023-06-13 16:31:00 +0900

    演習2

--- ex01-20230613/pygame_image.py
+++ ex01-20230613/pygame_image.py
@@ -7,10 +7,12 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
+    bgReverse_img = pg.transform.flip(bg_img, True, False)
     koukaton_img = pg.image.load("ex01-20230613/fig/3.png")
     koukatonReverse_img = pg.transform.flip(koukaton_img, True, False)
     koukatonReverseRotate10_img = [koukatonReverse_img, pg.transform.rotate(koukatonReverse_img, 10)]
     x = 0
+    x2 = 0
 
     tmr = 0
 
@@ -18,16 +20,21 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        if x >= 1600:
-            x -= 1600
+        
         
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(bgReverse_img, [1600-x2, 0])
+        screen.blit(bg_img, [3200-x2, 0])
         screen.blit(koukatonReverseRotate10_img[tmr % 2], [300, 200])
         pg.display.update()
         tmr += 1
-        x += 1     
-        clock.tick(100)
+        x += 1
+        if x >= 1600:
+            x = 0
+        x2 += 1
+        if x2 >= 3200:
+            x2 -= 3200     
+        clock.tick(1000)
 
 
 
